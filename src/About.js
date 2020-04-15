cf3a604def88a7d5619eea2a03b4d982da3a72f9
rakhid16 <roploverz@gmail.com>
2020-04-15 09:18:12 +0700

    fixed

--- src/About.js
+++ src/About.js
@@ -8,13 +8,13 @@ class About extends Component{
         <center>
         <img class="responsive" src="https://i.imgur.com/cGdkpld.jpg"/></center>
         <p align="center">
-          Hi! I'm Ikal Python Developer from Jombang. I'm a monotheist who interests in Data Science, Software Engineering, and Islamic Education as well. I also love linux and open source. We're supposed to make a biography page isn't? Well just go through contact and click my linkedin profile and you will get my curiculum vitae.
-          Now i just want to tell you, mates. 
-          </p>     <center><img class="responsive" src="https://i.pinimg.com/originals/ca/1f/74/ca1f746d6f232f87fca4e4d94ef6f3ab.png"/></center>
-  <p align="center">
-  The first question that is probably coming inside your mind is “Why specifically learn Python?” Well, the answer to this question is quite simple. Think about the factors you would like to include in a programming language.
-  <b>Learnability</b>, <b>Increased Productivity</b>, and <b>High paying salaries</b>. You can choose Python for Web Development, Data Science, Artificial Intelligence, Scripting, Scientific Computing and so on. So just learn GoLang then!
-        </p>
+        &emsp;&emsp;&emsp;Hi! I'm Ikal Python Developer from Jombang. I'm a monotheist who interests in Data Science, Software Engineering, and Islamic Education as well. I also love linux and open source. We're supposed to make a biography page isn't? Well just go through contact and click my linkedin profile and you will get my curiculum vitae.
+          Now i just want to tell you, <b>Why You Should Learn JavaScript Principles First (Not the Hottest Frameworks)</b>.<br/> <center> Because that is bad learning path</center><br/>
+          &emsp;&emsp;&emsp;As a Python programmer learn another language is not hard at all you might be just need an adaptation at first and the rest will be fine. The smiliarity between Python and JavaScript is they both has a lot of open source framework. 
+          Change one framework to another framework in Python for me not difficult as I am code in JavaScript. Ideally I'm not having skill in JS like my skill in Python (i hope you get what i mean). So when i change one framework to another JS framework it is confussing. Well I wont being a front-end dev, seriously. So straight learn the framework without prior knowledge of it's programming language was a bad choice.
+          Yesterday you were learning jQuery, then AngularJS and now ReactJS, Spring.js, Vue.js. Today, up comes Ext JS and Aurelia, the new ones. And tomorrow another will come up. The framework array list is endless.
+          Don’t chase the hottest new JavaScript frameworks or libraries. First, invest your invaluable time in understanding and focusing on the engine: the core concepts and Javascript principles. It same for all programming language. JavaScript world is like, you know, child who like screaming and running inside the house in the midnight. Little joking
+          </p>
       </div>
     );
   }
--- src/Contact.js
+++ src/Contact.js
@@ -4,10 +4,13 @@ class Contact extends Component {
   render() {
     return (
       <div>
-        <h2>Contact</h2>
-        <p>telegram : t.me/rakhmanWahid<br/>
-        Phone : 14045
-        </p>
+        <h2>Contact</h2><br/>
+        <img className="gambar" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/600px-Telegram_logo.svg.png" />
+        <a target="blank" href="https://t.me/rakhmanWahid">Telegram</a><br/>
+        <img className="gambar" src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-linkedin-circle-512.png" />
+        <a target="blank" href="https://linkedin.com/in/rakhid16">linkedIn</a><br/>
+        <img className="gambar" src="https://cdn.iconscout.com/icon/free/png-256/github-153-675523.png" />
+        <a target="blank" href="https://github.com/rakhid16">Github</a><br/>
       </div>
     );
   }
